12ea52488869dcdfbd407dbc0f25dde6960a4778
Luis D. Pedrosa <luis.pedrosa@usc.edu>
2016-01-04 16:58:41 -0800

    Updated analysis script.

--- run-spa.sh
+++ run-spa.sh
@@ -3,6 +3,7 @@
 set -e
 
 rm -f redis.paths
+rm -f dump.rdb
 
 # gdb --args \
 spa-explore \
@@ -13,6 +14,7 @@ spa-explore \
     --connect-sockets \
     --start-from spa_entry \
     --toward redis_done \
+    --stop-at redis_done \
     --output-at spa_msg_no_input_point \
     --output-at redis_done \
     --participant redis-client \
@@ -28,13 +30,12 @@ spa-explore \
     --connect-sockets \
     --start-from spa_entry \
     --toward spa_msg_output_point \
+    --stop-at spa_msg_select_no_input_point \
     --use-shallow-distance \
     --output-at spa_msg_output_point \
     --participant redis-server \
-    --dump-cov-on-interrupt redis-server.cov \
-    src/redis-server \
+    redis-server-llvm \
     2>&1 | tee redis-server.log &
-CLIENT_PID=$!
 
 trap 'kill $(jobs -p)' EXIT
 
