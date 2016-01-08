23aba9529558a15b885137cefd79fdcc7494e0dc
Luis D. Pedrosa <luis.pedrosa@usc.edu>
2016-01-07 20:12:45 -0800

    Added parallel analysis script.

---
+++ parallel-spa.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -e
+
+parallel-conversational-analysis.sh redis.paths \
+  "--connect-sockets --start-from spa_entry --toward redis_done --stop-at redis_done --output-at spa_msg_no_input_point --output-at redis_done --participant redis-client redis/spa-client.bc; \
+   --connect-sockets --start-from spa_entry --toward spa_msg_output_point --stop-at spa_msg_select_no_input_point --use-shallow-distance --output-at spa_msg_output_point --participant redis-server redis/redis-server-llvm"
+
+trap 'kill $(jobs -p)' EXIT
+
+wait
