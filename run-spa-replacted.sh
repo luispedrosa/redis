b9ddf2575e5d42d590972cc85b91787591ab865b
Luis D. Pedrosa <luis.pedrosa@usc.edu>
2016-01-08 20:22:27 -0800

    Updated analysis script for redis with replication.

--- run-spa-replacted.sh
+++ run-spa-replacted.sh
@@ -5,37 +5,37 @@ set -e
 rm -f redis-replicated.paths
 rm -f dump.rdb
 
-# # gdb --args \
-# spa-explore \
-#     --in-paths redis-replicated.paths \
-#     --follow-in-paths \
-#     --out-paths redis-replicated.paths \
-#     --out-paths-append \
-#     --connect-sockets \
-#     --start-from spa_entry \
-#     --toward redis_done \
-#     --stop-at redis_done \
-#     --output-at spa_msg_no_input_point \
-#     --output-at redis_done \
-#     --participant redis-client \
-#     spa-client.bc \
-#     2>&1 | tee redis-client.log &
+# gdb --args \
+spa-explore \
+    --in-paths redis-replicated.paths \
+    --follow-in-paths \
+    --out-paths redis-replicated.paths \
+    --out-paths-append \
+    --connect-sockets \
+    --start-from spa_entry \
+    --toward redis_done \
+    --stop-at redis_done \
+    --output-at spa_msg_no_input_point \
+    --output-at redis_done \
+    --participant redis-client \
+    spa-client.bc \
+    2>&1 | tee redis-client.log &
 
-# # gdb --args \
-# spa-explore \
-#     --in-paths redis-replicated.paths \
-#     --follow-in-paths \
-#     --out-paths redis-replicated.paths \
-#     --out-paths-append \
-#     --connect-sockets \
-#     --start-from spa_entry \
-#     --toward spa_msg_output_point \
-#     --stop-at spa_msg_select_no_input_point \
-#     --use-shallow-distance \
-#     --output-at spa_msg_output_point \
-#     --participant redis-master \
-#     redis-server-llvm \
-#     2>&1 | tee redis-master.log &
+# gdb --args \
+spa-explore \
+    --in-paths redis-replicated.paths \
+    --follow-in-paths \
+    --out-paths redis-replicated.paths \
+    --out-paths-append \
+    --connect-sockets \
+    --start-from spa_entry_master \
+    --toward spa_msg_output_point \
+    --stop-at spa_msg_select_no_input_point \
+    --use-shallow-distance \
+    --output-at spa_msg_output_point \
+    --participant redis-master \
+    redis-server-llvm \
+    2>&1 | tee redis-master.log &
 
 # gdb --args \
 spa-explore \
