ab808384d7146ae0af98e7d268c0377aa9a70ffa
Luis D. Pedrosa <luis.pedrosa@usc.edu>
2016-01-16 13:36:07 -0800

    Prepared 3-way master-slave replication analysis.

--- spa-client.c
+++ spa-client.c
@@ -23,7 +23,7 @@ void __attribute__((noinline, weak)) redis_done() {
   i++;
 }
 
-void spa_entry() {
+void spa_entry_single() {
   char set_value[2] = "v";
   spa_api_input_var(set_value);
   set_value[sizeof(set_value) - 1] = '\0';
@@ -55,7 +55,43 @@ void spa_entry() {
   redisFree(context);
 }
 
+void spa_entry_masterslave() {
+  char set_value[2] = "v";
+  spa_api_input_var(set_value);
+  set_value[sizeof(set_value) - 1] = '\0';
+
+  redisContext *masterContext = redisConnect("127.0.0.1", 6379);
+  assert(masterContext);
+
+  redisReply *reply = redisCommand(masterContext, "SET k %s", set_value);
+  assert(reply && reply->type == REDIS_REPLY_STATUS);
+#ifndef ENABLE_KLEE
+  printf("%s\n", reply->str);
+#endif
+  freeReplyObject(reply);
+  redisFree(masterContext);
+
+  redisContext *slaveContext = redisConnect("127.0.0.1", 6380);
+  assert(slaveContext);
+
+  reply = redisCommand(slaveContext, "GET k");
+  assert(reply && reply->type == REDIS_REPLY_STRING);
+#ifndef ENABLE_KLEE
+  printf("%s\n", reply->str);
+#endif
+
+  if (strcmp(set_value, reply->str) == 0) {
+    redis_sucess();
+  } else {
+    redis_fail();
+  }
+  redis_done();
+
+  freeReplyObject(reply);
+  redisFree(slaveContext);
+}
+
 int main(int argc, char *argv[]) {
-  spa_entry();
+  spa_entry_masterslave();
   return 0;
 }
