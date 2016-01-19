68048f4631ada7ce04ce44032592b90d8f1fa638
Luis D. Pedrosa <luis.pedrosa@usc.edu>
2016-01-18 19:38:28 -0800

    Adjusted stop/output points.

--- parallel-spa-replicated.sh
+++ parallel-spa-replicated.sh
@@ -6,8 +6,8 @@ rm -f dump.rdb
 
 parallel-conversational-analysis.sh redis-replicated.paths \
   "--connect-sockets --start-from spa_entry_masterslave --toward redis_done           --stop-at redis_done                                           --output-at spa_msg_no_input_point --output-at redis_done --participant redis-client redis/spa-client.bc; \
-   --connect-sockets --start-from spa_entry_master      --toward spa_msg_output_point --stop-at spa_msg_select_no_input_point --use-shallow-distance --output-at spa_msg_output_point                          --participant redis-master redis/redis-server-llvm; \
-   --connect-sockets --start-from spa_entry_slave       --toward spa_msg_output_point --stop-at spa_msg_output_point          --use-shallow-distance --output-at spa_msg_output_point                          --participant redis-slave  redis-server-llvm"
+   --connect-sockets --start-from spa_entry_master      --toward spa_msg_output_point --stop-at spa_msg_select_no_input_point --use-shallow-distance --output-at spa_msg_select_no_input_point                 --participant redis-master redis/redis-server-llvm; \
+   --connect-sockets --start-from spa_entry_slave       --toward spa_msg_output_point --stop-at spa_msg_select_no_input_point --use-shallow-distance --output-at spa_msg_select_no_input_point                 --participant redis-slave  redis-server-llvm"
 
 trap 'kill $(jobs -p)' EXIT
 
