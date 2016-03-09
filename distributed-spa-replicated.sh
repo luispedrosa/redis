6a1815053cc6993812106d8c448f80c09e62e799
Luis D. Pedrosa <luis.pedrosa@usc.edu>
2016-03-08 18:41:08 -0800

    Use new convention for specifying how to direct conversations. Direct towards two relevant conversations (one where slave syncs before master gets client request and one where they sync up after). Unify logs.

--- distributed-spa-replicated.sh
+++ distributed-spa-replicated.sh
@@ -2,16 +2,16 @@
 
 set -e
 
-TARGET_CONVERSATION="redis-slave redis-master redis-slave redis-master "\
-"redis-slave redis-master redis-slave redis-master redis-client redis-master "\
-"redis-client redis-slave redis-client"
+TARGET_CONVERSATION="redis-slave redis-master redis-slave redis-master redis-slave redis-master redis-slave redis-master redis-client redis-master redis-slave redis-master redis-client redis-slave redis-client; \
+                     redis-client redis-master redis-slave redis-master redis-slave redis-master redis-slave redis-master redis-slave redis-master redis-client redis-slave redis-client"
 
 trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM EXIT
 
 distributed-conversational-analysis.sh redis-replicated.paths \
-  "--shallow-exploration --filter-input \"NOT REACHED redis_client_done\" --connect-sockets --start-from spa_entry_masterslave --toward redis_client_done    --towards-conversation \"$TARGET_CONVERSATION\" --stop-at redis_client_done                        --output-at spa_msg_no_input_point --output-at redis_client_done --participant redis-client --ip 127.0.0.1 redis/spa-client.bc; \
-   --shallow-exploration --filter-input \"NOT REACHED redis_client_done\" --connect-sockets --start-from spa_entry_master      --toward spa_msg_output_point --towards-conversation \"$TARGET_CONVERSATION\" --stop-at redis_server_done --use-shallow-distance --output-at redis_server_done                                    --participant redis-master --ip 127.0.0.2 redis/redis-server-llvm; \
-   --shallow-exploration --filter-input \"NOT REACHED redis_client_done\" --connect-sockets --start-from spa_entry_slave       --toward spa_msg_output_point --towards-conversation \"$TARGET_CONVERSATION\" --stop-at redis_server_done --use-shallow-distance --output-at redis_server_done                                    --participant redis-slave  --ip 127.0.0.3 redis/redis-server-llvm" &
+  "--shallow-exploration --filter-input \"NOT REACHED redis_client_done\" --connect-sockets --start-from spa_entry_masterslave --toward redis_client_done    --stop-at redis_client_done                        --output-at spa_msg_no_input_point --output-at redis_client_done --participant redis-client --ip 127.0.0.1 redis/spa-client.bc; \
+   --shallow-exploration --filter-input \"NOT REACHED redis_client_done\" --connect-sockets --start-from spa_entry_master      --toward spa_msg_output_point --stop-at redis_server_done --use-shallow-distance --output-at redis_server_done                                    --participant redis-master --ip 127.0.0.2 redis/redis-server-llvm; \
+   --shallow-exploration --filter-input \"NOT REACHED redis_client_done\" --connect-sockets --start-from spa_entry_slave       --toward spa_msg_output_point --stop-at redis_server_done --use-shallow-distance --output-at redis_server_done                                    --participant redis-slave  --ip 127.0.0.3 redis/redis-server-llvm" \
+   "$TARGET_CONVERSATION" &
 SPA_PID=$!
 
 sleep 1
@@ -24,7 +24,7 @@ spa-explore-conversation \
     --participant-ip redis-slave:127.0.0.3 \
     --follow-in-paths \
     redis-replicated.paths \
-    > derived.log 2>&1 &
+    >>redis-replicated.paths.log 2>&1 &
 
 echo "Follow analysis progress on: http://localhost:8000/"
 spa-doc --serve-http 8000 \
