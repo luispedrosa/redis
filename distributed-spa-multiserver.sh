ce0e0f3a0b904f11a3be490bf21bf1ce94669fdc
Luis D. Pedrosa <luis.pedrosa@usc.edu>
2016-05-05 12:16:07 -0700

    Fixed result file name in multiserver analysis script. Updated the target conversation to facilitate a full resync when changing masters.

--- distributed-spa-multiserver.sh
+++ distributed-spa-multiserver.sh
@@ -2,11 +2,11 @@
 
 set -e
 
-TARGET_CONVERSATION="redis-client redis-master-a redis-client redis-master-b redis-client redis-slave-a redis-client redis-slave-b redis-client redis-slave-a redis-client redis-slave-b redis-client"
+TARGET_CONVERSATION="redis-client redis-master-a redis-client redis-master-b redis-client redis-slave-a redis-master-a redis-slave-a redis-master-a redis-slave-a redis-master-a redis-slave-a redis-master-a redis-client redis-slave-b redis-slave-a redis-slave-b redis-slave-a redis-slave-b redis-slave-a redis-slave-b redis-slave-a redis-client redis-slave-a redis-master-b redis-slave-a redis-master-b redis-slave-a redis-master-b redis-slave-a redis-master-b redis-slave-b redis-slave-a redis-client redis-slave-b redis-client"
 
 trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM EXIT
 
-distributed-conversational-analysis.sh redis-replicated.paths \
+distributed-conversational-analysis.sh redis-multiserver.paths \
   "--shallow-exploration --filter-input \"NOT REACHED redis_client_done\" --connect-sockets --start-from spa_entry_multiserver --toward redis_client_done    --stop-at redis_client_done                        --output-at spa_msg_output_point --output-at redis_client_done --participant redis-client   --ip 127.0.0.1 redis/spa-client.bc; \
    --shallow-exploration --filter-input \"NOT REACHED redis_client_done\" --connect-sockets --start-from spa_entry_master      --toward spa_msg_output_point --stop-at redis_server_done --use-shallow-distance --output-at spa_msg_output_point                               --participant redis-master-a --ip 127.0.0.2 redis/redis-server-llvm; \
    --shallow-exploration --filter-input \"NOT REACHED redis_client_done\" --connect-sockets --start-from spa_entry_master      --toward spa_msg_output_point --stop-at redis_server_done --use-shallow-distance --output-at spa_msg_output_point                               --participant redis-master-b --ip 127.0.0.3 redis/redis-server-llvm; \
@@ -26,8 +26,8 @@ spa-explore-conversation \
     --participant-ip redis-slave-a:127.0.0.4 \
     --participant-ip redis-slave-b:127.0.0.5 \
     --follow-in-paths \
-    redis-replicated.paths \
-    >>redis-replicated.paths.log 2>&1 &
+    redis-multiserver.paths \
+    >>redis-multiserver.paths.log 2>&1 &
 
 echo "Follow analysis progress on: http://localhost:8000/"
 spa-doc --serve-http 8000 \
@@ -35,7 +35,7 @@ spa-doc --serve-http 8000 \
         --color-filter "lightgreen:REACHED redis_success" \
         --color-filter "orangered:REACHED redis_fail" \
         --color-filter "cyan:CONVERSATION $(echo $TARGET_CONVERSATION | sed 's/ *; */ OR CONVERSATION /')" \
-        redis-replicated.paths
+        redis-multiserver.paths
 
 wait $SPA_PID
 echo "Analysis is complete. Ctrl-C when ready."
